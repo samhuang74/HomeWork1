6e6e7dae07817da20e07dfc21e2c55ceb711d3b0
samhuang74 <sam_huang74@hotmail.com>
2019-06-24 16:16:27 +0800

    add Question Class

---
+++ HomeWork1/Controllers/QuestionsController.cs
@@ -0,0 +1,124 @@
+﻿using HomeWork1.Models.VModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Utils;
+
+namespace HomeWork1.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        // GET: Questions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Questions/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Questions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Questions/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Questions/Edit/5
+        public ActionResult Edit(int id)
+        {
+            List<Question> qs = new List<Question>();
+
+            for (int qsIndex = 0; qsIndex < 10; qsIndex++)
+            {
+                Question q = new Question();
+
+                String idx = qsIndex.ToString();
+
+                q.Kind = "1";
+                q.QId = idx;
+                q.Topic = "test" + idx;
+
+                List<Option> options = new List<Option>();
+
+                for (int index = 0; index < 4; index++)
+                {
+                    Option op = new Option();
+
+                    op.Title = "選擇" + index;
+                    op.Value = StringUtils.getToString(index);
+
+                    //if (index % 2 == 0)
+                    //{
+                    //    op.IsChecked = true;
+                    //}
+
+                    options.Add(op);
+                }
+
+                q.Options = options;
+
+                qs.Add(q);
+            }
+            return View(qs);
+        }
+
+        // POST: Questions/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Questions/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Questions/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ HomeWork1/Models/VModels/Question.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork1.Models.VModels
+{
+    public class Question
+    {
+        [Display(Name = "ID")]
+        public String QId { get; set; }
+
+        [Display(Name = "類型")]
+        public String Kind { get; set; }
+
+        [Display(Name = "題目")]
+        public String Topic { get; set; }
+
+        [Display(Name = "選擇")]
+        public List<Option> Options { get; set; }
+    }
+
+    public class Option
+    {
+        public String Title { get; set; }
+        public String Value { get; set; }
+        public Boolean IsChecked { get; set; }
+    }
+}
