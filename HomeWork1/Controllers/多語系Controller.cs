abf7ab0f4c4f9acfb1dd97d99327d67b84b5c818
samhuang74 <sam_huang74@hotmail.com>
2019-06-28 16:45:48 +0800

    多語系

---
+++ HomeWork1/Controllers/多語系Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeWork1.Controllers
+{
+    public class 多語系Controller : Controller
+    {
+        // GET: 多語系
+        public ActionResult Index(String lang = "")
+        {
+            //設定使用語系
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lang);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+            return View();
+        }
+    }
+}
