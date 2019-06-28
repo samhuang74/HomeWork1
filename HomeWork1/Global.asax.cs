20d610a6e3e39e93fe6a9000e447e53361066528
samhuang74 <sam_huang74@hotmail.com>
2019-06-28 17:19:04 +0800

    多語系 測試

--- HomeWork1/Global.asax.cs
+++ HomeWork1/Global.asax.cs
@@ -3,6 +3,7 @@ using Repositories;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Hosting;
@@ -28,6 +29,8 @@ namespace HomeWork1
         protected void Application_BeginRequest()
         {
             SetDbHTTPContext();
+
+            SetLanguage(Request);
         }
 
         private static void SetDbHTTPContext()
@@ -39,5 +42,49 @@ namespace HomeWork1
             //};
             HttpContext.Current.Items[Repositories.FieldKey.DB_CONTEXT_KEY] = dbContext;
         }
+
+        private static void SetLanguage(HttpRequest Request)
+        {
+            CultureInfo ci = null;
+            try
+            {
+                //有request 才要切換 
+                String lan = Request.Cookies["ntustLan"].Value;
+                if (!String.IsNullOrEmpty(lan))
+                {
+
+                }
+
+                //取得用戶端語言喜好設定(已排序的字串陣列)
+                var userLanguages = Request.UserLanguages;
+
+                if (userLanguages.Length > 0)
+                {
+                    try
+                    {
+                        ci = new CultureInfo(userLanguages[0]);
+                    }
+                    catch (CultureNotFoundException)
+                    {
+                        ci = CultureInfo.InvariantCulture;
+                    }
+                }
+                else
+                {
+                    ci = CultureInfo.InvariantCulture;
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                System.Threading.Thread.CurrentThread.CurrentUICulture = ci;
+                System.Threading.Thread.CurrentThread.CurrentCulture = ci;
+            }
+        }
     }
+
 }
